4e80256aa670fb4fb1022ed05d8429341c684323
toby959 <134654437+toby959@users.noreply.github.com>
2024-06-18 19:47:19 -0300

    Delete product-crud-demo directory

--- product-crud-demo/src/main/java/com/example/demo/repo/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.demo.entity.Product;
-
-public interface ProductRepository extends MongoRepository<Product, Long>{
-
-}
